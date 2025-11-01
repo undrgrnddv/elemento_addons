040f46a90b255e82fbf830dd95296928532c8499
CC <c@elementorural.com>
2025-11-01 15:35:54 -0600

    Initial commit of local project files

---
+++ elemento_servidor/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Elemento ERP Configuracion de Servidor',
+    'version': '1.0',
+    'summary': 'Instala Modulos de Servidor',
+    'category': 'Elemento Custom',
+    'depends': [
+        'base_fontawesome',
+        'auto_backup',
+        'document_knowledge',
+        'document_page',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ elemento_ui/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Elemento ERP Interfaz de Usuario',
+    'version': '1.0',
+    'summary': 'Instala Modulos de Interfaz de Usuario.',
+    'category': 'Elemento Custom',
+    'depends': [
+        'web_quick_start_screen',
+        'account_reconcile_model_oca',
+        'web_responsive',
+        'web_remember_tree_column_width',
+        'web_no_bubble',
+        'web_m2x_options',
+        'disable_odoo_online',
+        'portal_odoo_debranding',
+        'remove_odoo_enterprise',
+    ],
+    'installable': True,
+    'application': True,
+}
